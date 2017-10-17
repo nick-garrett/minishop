d9b9884f382d1e1f650d3c3bcca1e59ad9acf25c
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-17 21:28:07 +1300

    Added reject statement that will fail validation if the address doesn't have all fields. Also removed unnecessary attr_accessor on validated.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,20 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   has_one :address
-  accepts_nested_attributes_for :address, allow_destroy: true
+  accepts_nested_attributes_for :address, allow_destroy: true, reject_if: :all_blank
 
   validates_associated :address
-  # validates_presence_of :address
+  validates_presence_of :address
   validates :icp, presence: true, uniqueness: true, length: { minimum: 9, maximum: 9 }
   validates :first_name, presence: true, length: { maximum: 50 }
   validates :last_name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
 
   before_save { email&.downcase! }
 
   has_secure_password
+
+  # attr_accessor :validated
 end
