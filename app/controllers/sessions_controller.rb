b72f151c790c6cd3e4b3ce733e00773849bbada9
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-17 22:12:38 +1300

    Added the new, create, and destryo methods that signify loggin in and logging out.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      @current_user = user
+      redirect_to user
+    else
+      flash[:error] = 'Login credentials did not match'
+      redirect_to '/login'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    @current_user = nil
+    redirect_to '/login'
+  end
 end
