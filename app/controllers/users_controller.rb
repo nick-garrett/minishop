0c6fd4554a69cc6b707cbe353b4c852d479a92dd
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-17 22:13:05 +1300

    Added logic for redirecting if the incorrect user is logged in.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,22 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    @current_user ||= User.find_by(id: session[:user_id])
+    redirect_to root_path unless @current_user&.admin?
   end
 
   def show
     @user = User.find(params[:id])
+    @current_user ||= User.find_by(id: session[:user_id])
+    if @user != @current_user
+      redirect_to '/login'
+    end
   end
 
   def create
     user = User.new(user_params)
     if user.save
+      session[:user_id] = user.id
       redirect_to user
     else
       flash[:error] = user.errors.full_messages.first
