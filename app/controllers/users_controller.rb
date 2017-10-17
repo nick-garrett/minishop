25e1feadf56de7a4540f9742715491dffb22f403
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-18 12:17:32 +1300

    Made user an ivar so can test using assigns.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,12 +15,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to user
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user
     else
-      flash[:error] = user.errors.full_messages.first
+      flash[:error] = @user.errors.full_messages.first
       redirect_to new_user_url
     end
   end
