28e967bcef33ab9e47f2c037078d170dc7cc0a46
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-17 21:26:49 +1300

    Set the users ivar in the index method so that all users can be displayed. Remove the create! and replace with new so that errors can be detected and added to the flash on save. Update and destroy methods are used so to update and destroy users. User params method tidied to make it more readable. Also added validated field and fixed the syntax for address_attributes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
@@ -7,16 +8,41 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.create!(user_params)
-    redirect_to user if user.save
+    user = User.new(user_params)
+    if user.save
+      redirect_to user
+    else
+      flash[:error] = user.errors.full_messages.first
+      redirect_to new_user_url
+    end
   end
 
   def new
     @user = User.new
+    @user.address = Address.new
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to users_path
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to users_path
   end
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :password, :password_confirmation, :email, :icp)#, :address_attributes[:line_1, :line_2, :line_3])
+    params.require(:user).permit(:first_name,
+     :last_name,
+     :password,
+     :password_confirmation,
+     :email,
+     :icp,
+     :validated,
+     address_attributes: [:line_1, :line_2, :line_3])
   end
 
   def address_params
