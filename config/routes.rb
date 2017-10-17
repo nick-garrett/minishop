4d9baa0a6299c1ba47d59cbba31cc20d46f5223f
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-17 22:17:03 +1300

    Added routes for loggin in and loggin out.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   resources :users do
     resource :addresses
   end
+
+  root 'sessions#new'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 end
