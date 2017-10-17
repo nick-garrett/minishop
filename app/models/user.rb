368e203e3f7e4e4da599fe9c6b7b4662d89c8311
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-17 14:25:27 +1300

    Tidying up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   has_one :address
+  accepts_nested_attributes_for :address, allow_destroy: true
 
-  validate_associated :address
-  validates_presence_of :address
+  validates_associated :address
+  # validates_presence_of :address
   validates :icp, presence: true, uniqueness: true, length: { minimum: 9, maximum: 9 }
   validates :first_name, presence: true, length: { maximum: 50 }
   validates :last_name, presence: true, length: { maximum: 50 }
