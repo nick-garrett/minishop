da8e4ae21b49c9af98fa7dc8ddea97f0695e993c
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-17 13:19:48 +1300

    Added has_many address, and validates the presence of the address.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,17 @@
 class User < ApplicationRecord
-  validate :address, presence: true, uniqueness: true
-  validate :icp, presence: true, uniqueness: true
-  validate :first_name, presence: true, length: { maximum: 50 }
-  validate :last_name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  has_one :address
+
+  validate_associated :address
+  validates_presence_of :address
+  validates :icp, presence: true, uniqueness: true
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :last_name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 
   before_save { email&.downcase! }
-
-  # TODO: Validate address format, length??? Should address be a model?(probably not)
-
-  # Address format
-  # street address
-  # suburb
-  # city
 
   has_secure_password
 end
