fc28710b415ec12f9a9534034e3c5d4025165fad
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-17 13:21:00 +1300

    Added migration for the addresses db.

--- db/migrate/20171017000525_create_addresses.rb
+++ db/migrate/20171017000525_create_addresses.rb
@@ -1,7 +1,9 @@
 class CreateAddresses < ActiveRecord::Migration[5.1]
   def change
     create_table :addresses do |t|
-
+      t.text :line_1
+      t.text :line_2
+      t.text :line_3
       t.timestamps
     end
   end
