6d1fa2b23b25773dc8070b2a66d602dca705658d
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-18 14:50:29 +1300

    Add tests for POST #create under circumstances were a user is already logged in, and isn't logged in, with correct and incorrect user details.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -34,8 +34,57 @@ RSpec.describe SessionsController, type: :controller do
     let(:email) { user.email }
     let(:password) { 'password' }
 
-    before do
-      post :create, params: { session: { email: email, password: password } }
+    context 'when a user is already logged in' do
+      let(:users_count) { User.count }
+
+      before do
+        session[:user_id] = user.id
+        post :create, params: { session: { email: email, password: password } }
+      end
+
+      it 'redirects to the users page' do
+        expect(response).to redirect_to(user)
+      end
+
+      it 'does not increase the number of users' do
+        expect(users_count).to eq(User.count)
+      end
+    end
+
+    context 'when a user is not logged in' do
+      let(:email) { user.email }
+      let(:password) { 'password' }
+
+      before do
+        session[:user_id] = nil
+        post :create, params: { session: { email: email, password: password } }
+      end
+
+      context 'when non-matching credentials are submitted' do
+        let(:password) { 'invalid' }
+
+        it 'redirects to login page' do
+          expect(response).to redirect_to(login_url)
+        end
+
+        it 'adds error to flash' do
+          expect(flash[:error]).to eq 'Login credentials did not match'
+        end
+      end
+
+      context 'when matching credentials are submitted' do
+        it 'redirects to user page' do
+          expect(response).to redirect_to(user)
+        end
+
+        it 'does not add error to flash' do
+          expect(flash[:error]).not_to eq 'Login credentials did not match'
+        end
+
+        it 'sets the current user' do
+          expect(session[:user_id]).to eq user.id
+        end
+      end
     end
   end
 
