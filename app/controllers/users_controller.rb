d903dfdb0d14d7f37459bd3aa2f0e97a07179709
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-24 09:26:08 +1300

    Changed guard clause to use unless instead of an if negative.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @current_user ||= User.find_by(id: session[:user_id])
+
     redirect_to root_path unless @current_user&.admin?
   end
 
@@ -14,11 +15,12 @@ class UsersController < ApplicationController
       return
     end
 
-    redirect_to '/login' if @user != @current_user
+    redirect_to '/login' unless @user == @current_user
   end
 
   def create
     @user = User.new(user_params)
+
     if @user.save
       session[:user_id] = @user.id
       redirect_to @user
@@ -30,6 +32,7 @@ class UsersController < ApplicationController
 
   def new
     @current_user ||= User.find_by(id: session[:user_id])
+
     if @current_user
       redirect_to @current_user
       return
@@ -41,10 +44,12 @@ class UsersController < ApplicationController
   def update
     user = User.find(params[:id])
     @current_user ||= User.find_by(id: session[:user_id])
+
     unless @current_user&.admin?
       redirect_to root_path
       return
     end
+
     user.update(user_params)
     redirect_to users_path
   end
@@ -52,10 +57,12 @@ class UsersController < ApplicationController
   def destroy
     user = User.find(params[:id])
     @current_user ||= User.find_by(id: session[:user_id])
+
     unless @current_user&.admin?
       redirect_to root_path
       return
     end
+
     user.destroy
     redirect_to users_path
   end
