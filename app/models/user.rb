17ac1fce3f600e9bfa5077bd6582a2dc89584dd7
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-24 14:52:04 +1300

    Added relation to invoice

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   has_one :address
+  has_many :invoices
 
   accepts_nested_attributes_for :address, allow_destroy: true, reject_if: :all_blank
 
