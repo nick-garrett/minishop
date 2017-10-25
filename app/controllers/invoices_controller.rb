888a4d792d7b5c52395e67a87f7de1adcec9437f
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-25 13:52:17 +1300

    Add the before action methods to get current user and user.

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -31,4 +31,12 @@ class InvoicesController < ApplicationController
 
   def new
   end
+
+  def find_current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def find_user
+    @user = User.find_by(id: params[:user_id])
+  end
 end
