633b5586096986d34fd452d1199520972c743942
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-25 13:51:05 +1300

    Beginning of implementation of invoices controller, making it pass the tests written for index.

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,8 +1,29 @@
 class InvoicesController < ApplicationController
+  before_action :find_current_user
+  before_action :find_user
+
   def index
+    if @current_user&.id == @user&.id
+
+    elsif @current_user.nil?
+      redirect_to login_path
+      flash[:error] = 'You must be logged in to view invoices.'
+    else
+      redirect_to @current_user
+      flash[:error] = 'You cannot view invoices for another user.'
+    end
   end
 
   def show
+    if @current_user&.id == @user&.id
+      
+    elsif @current_user.nil?
+      redirect_to login_path
+      flash[:error] = 'You must be logged in to view invoices.'
+    else
+      redirect_to @current_user
+      flash[:error] = 'You cannot view invoices for another user.'
+    end
   end
 
   def create
