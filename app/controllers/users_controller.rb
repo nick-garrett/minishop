ad8a37c10afa4944b627643ac7cb2c947250787f
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-26 14:03:06 +1300

    Added before_action to check if the current user is an admin, so that actions are less crowded with if statements. Perhaps could tidy it up further with more. On the create form, using ajax to display errors without reloading the page, making for a nicer user experience.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,16 @@
 class UsersController < ApplicationController
   before_action :find_current_user, only: %i[index show new update destroy]
   before_action :find_user, only: %i[show create update destroy]
+  before_action :check_permission, only: %i[index update destroy]
 
   def index
     @users = User.all
-
-    redirect_to root_path unless @current_user&.admin?
   end
 
   def show
-    if @current_user&.admin?
-      redirect_to users_path
-    elsif !@current_user
+    if !@current_user
       redirect_to login_path
-      flash[:error] = 'You must be logged in to view this page'
+      flash[:error] = 'You must be logged in'
     elsif @user != @current_user
       redirect_to @current_user
       flash[:error] = 'You cannot view another users profile'
@@ -21,13 +18,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.new(create_user_params) # don't permit validated in user params
     if @user.save
       session[:user_id] = @user.id
       redirect_to @user
     else
-      flash[:error] = @user.errors.full_messages.first
-      redirect_to new_user_url
+      flash.now[:error] = @user.errors.full_messages
+      # TODO: form helper errors
+      # redirect_to new_user_url
+      respond_to do |format|
+        format.js
+      end
     end
   end
 
@@ -42,35 +43,34 @@ class UsersController < ApplicationController
   end
 
   def update
-    if @current_user&.admin?
-      @user.update(user_params)
-      redirect_to users_path
-    else
-      redirect_to root_path
-      flash[:error] = 'You must be an admin to update a user'
-    end
+    flash[:error] = 'Failed to update user' unless @user.update_attributes(user_validated_param)
+    redirect_to users_path
   end
 
+  # TODO: remove destroy
   def destroy
-    # admin can't destroy themselves
-    if @current_user&.admin?
-      @user.destroy unless @user.admin?
-      redirect_to users_path
-    else
-      redirect_to root_path
-      flash[:error] = 'You must be an admin to delete a user'
-    end
+    @user.destroy unless @user.admin?
+    redirect_to users_path
   end
 
-  def user_params
+  def create_user_params
     params.require(:user).permit(:first_name,
-                                 :last_name,
-                                 :password,
-                                 :password_confirmation,
-                                 :email,
-                                 :icp,
-                                 :validated,
-                                 address_attributes: %i[line_1 line_2 line_3])
+     :last_name,
+     :password,
+     :password_confirmation,
+     :email,
+     :icp,
+     address_attributes: %i[line_1 line_2 line_3])
+  end
+
+  def user_validated_param
+    params.require(:user).permit(:validated)
+  end
+
+  def check_permission
+    return if @current_user&.admin?
+    redirect_to root_path
+    flash[:error] = 'You must be an admin'
   end
 
   def find_current_user
