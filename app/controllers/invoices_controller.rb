c1548a8def72096ef16c4dd90d294d3a623e853d
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-26 13:59:22 +1300

    reordered if statements to make them look nicer. Need to move authorisation into before_action hooks.

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -3,8 +3,8 @@ class InvoicesController < ApplicationController
   before_action :find_user
 
   def index
-    if @current_user&.id == @user&.id
-
+    if @current_user == @user || @current_user&.admin?
+      @invoices = @user.invoices
     elsif @current_user.nil?
       redirect_to login_path
       flash[:error] = 'You must be logged in to view invoices.'
@@ -15,9 +15,9 @@ class InvoicesController < ApplicationController
   end
 
   def show
-    if @current_user&.id == @user&.id
-      invoice = @current_user.invoices.find_by(id: params[:id])
-      unless invoice
+    if @current_user == @user || @current_user&.admin?
+      @invoice = @user.invoices.find_by(id: params[:id])
+      unless @invoice
         redirect_to @current_user
         flash[:error] = "Invoice doesn't exist."
       end
@@ -33,24 +33,29 @@ class InvoicesController < ApplicationController
   def create
     if @current_user.nil?
       redirect_to login_path
-      flash[:error] = 'You must be logged in to create an invoice.'
-    elsif @user != @current_user
-      redirect_to users_path(@current_user)
-      flash[:error] = 'You cannot create an invoice for another user.'
+      flash[:error] = 'You must be logged in as an admin to create an invoice.'
+    elsif @current_user&.admin?
+      @invoice = @user.invoices.create(invoice_params)
+      redirect_to user_invoice_url(@user.id, @invoice) if @invoice.valid?
+      # flash error if something failed?
     else
-      # create a new invoice somehow
-      render 'index'
+      redirect_to users_path(@current_user)
+      flash[:error] = 'You must be logged in as an admin to create an invoice.'
     end
   end
 
   def new
     if @current_user.nil?
       redirect_to login_path
-      flash[:error] = 'You must be logged in to create an invoice.'
-    elsif @user != @current_user
+      flash[:error] = 'You must be logged in as an admin to create an invoice.'
+    elsif !@current_user.admin?
       redirect_to users_path(@current_user)
-      flash[:error] = 'You cannot create an invoice for another user.'
+      flash[:error] = 'You must be logged in as an admin to create an invoice.'
     end
+  end
+
+  def invoice_params
+    params.require(:invoice).permit(:price, :usage)
   end
 
   def find_current_user
